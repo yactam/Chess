39afdf05a1ab3fdf8d149fb6268ba5be6d6c465a
TAMDRARI Yacine <yacinetamdrari5@gmail.com>
2023-03-01 11:31:08 +0100

    player class

--- src/com/chess/engine/player/BlackPlayer.java
+++ src/com/chess/engine/player/BlackPlayer.java
@@ -2,10 +2,17 @@ package com.chess.engine.player;
 
 import com.chess.engine.board.Board;
 import com.chess.engine.board.Move;
+import com.chess.engine.pieces.Piece;
 
 import java.util.Collection;
 
-public class BlackPlayer {
+public class BlackPlayer extends Player {
     public BlackPlayer(Board board, Collection<Move> whiteStandardLegalMoves, Collection<Move> blackStandardLegalMoves) {
+        super(board, blackStandardLegalMoves, whiteStandardLegalMoves);
+    }
+
+    @Override
+    Collection<Piece> getActivePieces() {
+        return this.board.getBlackPieces();
     }
 }
--- src/com/chess/engine/player/Player.java
+++ src/com/chess/engine/player/Player.java
@@ -1,4 +1,36 @@
 package com.chess.engine.player;
 
-public class Player {
+import com.chess.engine.board.Board;
+import com.chess.engine.pieces.King;
+import com.chess.engine.board.Move;
+import com.chess.engine.pieces.Piece;
+
+import java.util.Collection;
+
+
+public abstract class Player {
+
+    protected final Board board;
+    protected final King king;
+    protected final Collection<Move> legalMoves;
+    protected final Collection<Move> opponentMoves;
+
+    Player(Board board, Collection<Move> legalMoves, Collection<Move> opponentMoves) {
+        this.king = establishKing();
+        this.board = board;
+        this.legalMoves = legalMoves;
+        this.opponentMoves = opponentMoves;
+    }
+
+    private King establishKing() {
+        for(Piece piece : getActivePieces()) {
+            if(piece.getPieceType().equals(Piece.PieceType.KING)) {
+                return (King) piece;
+            }
+        }
+        throw new RuntimeException("King is dead !!!!!!!!!");
+    }
+
+    abstract Collection<Piece> getActivePieces();
+
 }
